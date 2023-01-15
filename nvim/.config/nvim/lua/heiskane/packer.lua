4cae06984ac9101a0a4638d1bbf3e9ac4dd3c28d
heiskane <henipe@gmail.com>
2023-01-15 22:17:59 +0200

    nvim comments plugin

--- nvim/.config/nvim/lua/heiskane/packer.lua
+++ nvim/.config/nvim/lua/heiskane/packer.lua
@@ -24,6 +24,14 @@ return require('packer').startup(function(use)
   }
 
   use {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup{}
+    end
+  }
+
+  use {
   'VonHeikemen/lsp-zero.nvim',
   requires = {
     -- LSP Support
