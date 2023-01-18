ba54e8a67cfdab2190f5aaee4b1f2ae8ce62185d
heiskane <henipe@gmail.com>
2023-01-18 14:05:40 +0200

    fix lua lsp

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -2,7 +2,7 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
-lsp.ensure_installed({'eslint', 'rust_analyzer', 'pyright'})
+lsp.ensure_installed({'eslint', 'rust_analyzer', 'pyright', 'sumneko_lua'})
 
 lsp.on_attach(function(_, bufnr)
     local opts = {buffer = bufnr, remap = false}
@@ -18,10 +18,10 @@ lsp.on_attach(function(_, bufnr)
     vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
     vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
     vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
 
+lsp.nvim_workspace()
 lsp.setup()
 
 vim.diagnostic.config({
