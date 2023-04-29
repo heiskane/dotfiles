d779afa67873e21cc7f59a55b54a13e10d22cff4
heiskane <henipe@gmail.com>
2023-04-29 13:32:59 +0300

    format on query and query_as

--- nvim/.config/nvim/after/queries/rust/injections.scm
+++ nvim/.config/nvim/after/queries/rust/injections.scm
@@ -3,6 +3,6 @@
 (call_expression
   (scoped_identifier
     path: (identifier) @_path (#eq? @_path "sqlx")
-    name: (identifier) @_name (#eq? @_name "query"))
+    name: (identifier) @_name (#match? @_name "query(_as){0,1}"))
   (arguments (raw_string_literal) @sql)
-  (#offset! @sql 1 -1 0 0))
+  (#offset! @sql 1 0 -1 0))
