deace98e702989f7d081599e6a8987190d25a9c1
heiskane <niko.heiskanen@corefactory.com>
2023-08-04 16:02:42 +0300

    more explicit injection

--- nvim/.config/nvim/after/queries/bash/injections.scm
+++ nvim/.config/nvim/after/queries/bash/injections.scm
@@ -1,6 +1,6 @@
 ; extends
 
 ((redirected_statement
-  redirect: (heredoc_redirect (heredoc_start) @injection.language))
- (heredoc_body) @injection.content
+  redirect: (heredoc_redirect (heredoc_start) @_eof (#eq? @_eof "EOFSQL")))
+ (heredoc_body) @sql (#offset! @sql 0 0 -1 0)
 )
