871a5ea9ac8d09bfe9da793130713e81c404978c
heiskane <niko.heiskanen@corefactory.com>
2023-08-04 15:00:02 +0300

    injections

---
+++ nvim/.config/nvim/after/queries/bash/injections.scm
@@ -0,0 +1,6 @@
+; extends
+
+((redirected_statement
+  redirect: (heredoc_redirect (heredoc_start) @injection.language))
+ (heredoc_body) @injection.content
+)
---
+++ nvim/.config/nvim/after/queries/yaml/injections.scm
@@ -0,0 +1,15 @@
+; extends
+
+(block_mapping_pair
+  key: (flow_node) @_file_name (#match? @_file_name ".*\.sh")
+  value: (block_node (block_scalar) @bash
+    (#offset! @bash 0 1 0 0)
+  )
+)
+
+(block_mapping_pair
+  key: (flow_node) @_file_name (#match? @_file_name ".*\.conf")
+  value: (block_node (block_scalar) @ini
+    (#offset! @ini 0 1 0 0)
+  )
+)
