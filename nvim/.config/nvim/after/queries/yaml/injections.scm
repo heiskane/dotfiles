bd799e9c2c14be5f427edb719830f354fd0cd043
heiskane <niko.heiskanen@corefactory.com>
2023-09-01 11:30:44 +0300

    syntax highlighting for pyhon block_nodes in yaml

--- nvim/.config/nvim/after/queries/yaml/injections.scm
+++ nvim/.config/nvim/after/queries/yaml/injections.scm
@@ -13,3 +13,10 @@
     (#offset! @python 0 1 0 0)
   )
 )
+
+(block_mapping_pair
+  key: (flow_node) @_file_name (#match? @_file_name ".*\.py$")
+  value: (block_node (block_scalar) @python
+    (#offset! @python 0 1 0 0)
+  )
+)
