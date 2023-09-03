462d01fa61393734e3511e4f0f654ee11bd08919
heiskane <henipe@gmail.com>
2023-09-03 09:40:25 +0300

    nvim autopairs

--- nvim/.config/nvim/lua/heiskane/packer.lua
+++ nvim/.config/nvim/lua/heiskane/packer.lua
@@ -98,6 +98,11 @@ return require('packer').startup(function(use)
 
     use('airblade/vim-gitgutter')
 
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then require('packer').sync() end
