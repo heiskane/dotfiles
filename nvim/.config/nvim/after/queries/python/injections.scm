16fda833e04e76bcdda2b1f90a2b4352745127d3
Niko Heiskanen <niko.heiskanen@corefactory.com>
2024-01-08 11:24:28 +0200

    add injections for syntax highlighting session.execute calls with raw sql

--- nvim/.config/nvim/after/queries/python/injections.scm
+++ nvim/.config/nvim/after/queries/python/injections.scm
@@ -8,3 +8,30 @@
     (string
       (string_content) @sql)
 )))
+
+; async session.execute calls with raw sql
+(expression_statement
+  (await
+  (call
+  (attribute
+    object: (identifier) @_obj (#eq? @_obj "session")
+    attribute: (identifier) @_attr (#eq? @_attr "execute"))
+  (argument_list
+    (call (identifier) @_ident (#eq? @_ident "text")
+    (argument_list
+      (string 
+        (string_content) @sql))
+)))))
+
+; sync session.execute calls with raw sql
+(expression_statement
+  (call
+  (attribute
+    object: (identifier) @_obj (#eq? @_obj "session")
+    attribute: (identifier) @_attr (#eq? @_attr "execute"))
+  (argument_list
+    (call (identifier) @_ident (#eq? @_ident "text")
+    (argument_list
+      (string 
+        (string_content) @sql))
+))))
