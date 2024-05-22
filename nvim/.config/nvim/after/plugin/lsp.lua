642356825100cb7ca6bedceb40a5912ded44c6f5
heiskane <henipe@gmail.com>
2024-05-22 21:01:47 +0300

    fix(nvim): update lsp-zero to 3.X

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -5,9 +5,12 @@ require("fidget").setup()
 
 lsp.preset("recommended")
 
-lsp.set_preferences({ set_lsp_keymaps = { omit = { "<F2>" } } })
-
 lsp.on_attach(function(_, bufnr)
+	lsp.default_keymaps({
+		buffer = bufnr,
+		exclude = { "<F2>" },
+	})
+
 	local opts = { buffer = bufnr, remap = false }
 
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
