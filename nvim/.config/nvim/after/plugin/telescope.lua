043bb233f9aa035effa8a2cb5a14e6901c10151b
heiskane <henipe@gmail.com>
2024-05-22 21:02:40 +0300

    reformat

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,15 +1,9 @@
-require("telescope").setup {
-    extensions = {["ui-select"] = {require("telescope.themes").get_dropdown {}}}
-}
+require("telescope").setup({
+	extensions = { ["ui-select"] = { require("telescope.themes").get_dropdown({}) } },
+})
 require("telescope").load_extension("ui-select")
 
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
-vim.keymap.set('n', 'ff', builtin.find_files, {})
--- vim.keymap.set('n', '<leader>fg', function()
--- 	builtin.grep_string({ search = vim.fn.input("grep: ") });
--- end)
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set("n", '<leader>fr', builtin.lsp_references,
-               {noremap = true, silent = true})
-
+vim.keymap.set("n", "ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
