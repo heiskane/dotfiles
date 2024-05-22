2850a52a280369b217f5a047bef5ff5926754db1
Niko Heiskanen <niko.heiskanen@kuulto.com>
2024-05-22 13:54:21 +0300

    formatting and htmx lsp

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,42 +1,46 @@
-local lsp = require('lsp-zero')
+local lsp = require("lsp-zero")
+local lspconfig = require("lspconfig")
 
 require("fidget").setup()
--- require("ibl").setup()
 
-lsp.preset('recommended')
+lsp.preset("recommended")
 
-lsp.set_preferences({set_lsp_keymaps = {omit = {'<F2>'}}})
+lsp.set_preferences({ set_lsp_keymaps = { omit = { "<F2>" } } })
 
 lsp.on_attach(function(_, bufnr)
-    local opts = {buffer = bufnr, remap = false}
+	local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<space>r', vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>op", vim.diagnostic.open_float)
-    vim.keymap.set("n", 'dn', vim.diagnostic.goto_next)
-    vim.keymap.set("n", 'dp', vim.diagnostic.goto_prev)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+	vim.keymap.set("n", "<space>r", vim.lsp.buf.rename, opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "<leader>op", vim.diagnostic.open_float)
+	vim.keymap.set("n", "dn", vim.diagnostic.goto_next)
+	vim.keymap.set("n", "dp", vim.diagnostic.goto_prev)
 
-    vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, opts)
+	vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, opts)
 end)
 
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true,
-    signs = true,
-    update_in_insert = false,
-    underline = true,
-    severity_sort = false,
-    float = true
+	virtual_text = true,
+	signs = true,
+	update_in_insert = false,
+	underline = true,
+	severity_sort = false,
+	float = true,
 })
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
-	ensure_installed = { "pyright" },
+	ensure_installed = { "pyright", "markdown_oxide" },
 	handlers = {
 		lsp.default_setup,
+		html = function()
+			lspconfig.html.setup({})
+			lspconfig.htmx.setup({})
+		end,
 	},
 })
