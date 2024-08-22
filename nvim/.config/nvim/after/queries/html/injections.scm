af8430dd4d38cbed22b2682bb27aec86ab1df335
Niko Heiskanen <niko.heiskanen@kuulto.com>
2024-08-22 11:23:14 +0300

    add some mildly questionable injections

--- nvim/.config/nvim/after/queries/html/injections.scm
+++ nvim/.config/nvim/after/queries/html/injections.scm
@@ -5,3 +5,13 @@
   (attribute_name) @_attr (#match? @_attr "hx-on:.*")
   (quoted_attribute_value (attribute_value) @javascript)
 )
+
+; angular js highligting
+(attribute 
+  (attribute_name) @_attr (#match? @_attr "\*ngIf")
+  (quoted_attribute_value (attribute_value) @javascript)
+)
+(attribute 
+  (attribute_name) @_attr (#match? @_attr "\*ngFor")
+  (quoted_attribute_value (attribute_value) @javascript)
+)
