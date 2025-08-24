57b4433b431b2d3549df5141bb49225c2ef0cec4
Niko Heiskanen <henipe@gmail.com>
2025-08-24 11:44:03 +0300

    (nvim): fix treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
     build = ":TSUpdate",
     opts_extend = { "ensure_installed" },
     opts = {
+        highlight = { enable = true },
         ensure_installed = {
             "python",
             "elixir",
@@ -15,15 +16,9 @@ return {
             "markdown_inline",
             "lua",
         },
-        highlight = {
-            -- `false` will disable the whole extension
-            enable = true,
-
-            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-            -- Using this option may slow down your editor, and you may see some duplicate highlights.
-            -- Instead of true it can also be a list of languages
-            -- additional_vim_regex_highlighting = false,
-        },
     },
+  ---@param opts TSConfig
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
