cd9ba734ea5a76a71dd98d9aaab880672751b4c4
Niko Heiskanen <niko.heiskanen@kuulto.com>
2025-08-25 08:14:27 +0300

    nvim fix colorscheme

--- nvim/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -2,11 +2,8 @@ return {
     "folke/tokyonight.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-        style = "night",
-    },
     config = function()
         -- load the colorscheme here
-        vim.cmd([[colorscheme tokyonight]])
+        vim.cmd([[colorscheme tokyonight-night]])
     end,
 }
