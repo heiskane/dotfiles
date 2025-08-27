86741016ca1e9a9865ec433c047c72a3f2302d46
Niko Heiskanen <niko.heiskanen@kuulto.com>
2025-08-27 16:41:01 +0300

    (nvim): fix visual_multi breaking blink-cmp

--- nvim/.config/nvim/lua/plugins/visual_multi.lua
+++ nvim/.config/nvim/lua/plugins/visual_multi.lua
@@ -7,12 +7,32 @@ return {
             pattern = "visual_multi_exit",
             callback = function()
                 local cmp = require("blink.cmp")
+
+                -- TODO: Find a better way of fixing visual_multi breaking blink
                 vim.keymap.set("i", "<CR>", function()
                     if cmp.is_visible() then
                         cmp.accept()
                         return ""
                     else
                         return vim.api.nvim_replace_termcodes("<CR>", true, false, true)
+                    end
+                end, { expr = true, silent = true })
+
+                vim.keymap.set("i", "<Up>", function()
+                    if cmp.is_visible() then
+                        cmp.select_prev()
+                        return ""
+                    else
+                        return vim.api.nvim_replace_termcodes("<Up>", true, false, false)
+                    end
+                end, { expr = true, silent = true })
+
+                vim.keymap.set("i", "<Down>", function()
+                    if cmp.is_visible() then
+                        cmp.select_next()
+                        return ""
+                    else
+                        return vim.api.nvim_replace_termcodes("<Down>", true, false, false)
                     end
                 end, { expr = true, silent = true })
             end,
