e8a99983ffe301240bac77d80aae609b1c612822
Niko Heiskanen <henipe@gmail.com>
2025-08-28 19:24:49 +0300

    (nvim): `Gwip` command to yeet commit all changes

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,8 +1,16 @@
 return {
     "tpope/vim-fugitive",
-    cmd = { "Git" },
+    cmd = { "Git", "Gwip" },
     keys = {
         { "<leader>gi", "<cmd>vertical Git<cr>", mode = "n" },
         { "<leader>gl", "<cmd>vertical Git log --pretty=format:'%h %as %an %s'<CR>", mode = "n" },
     },
+    config = function()
+        vim.api.nvim_create_user_command("Gwip", function()
+            vim.cmd([[Git add --all]])
+            vim.cmd(string.format("Git commit -m '(wip): %s'", os.date("%Y-%m-%dT%H:%M")))
+        end, {
+            desc = "Create a `work_in_progress` commit",
+        })
+    end,
 }
