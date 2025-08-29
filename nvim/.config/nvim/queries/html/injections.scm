199632dce654b9030929aa17eb0af364a785459b
Niko Heiskanen <henipe@gmail.com>
2025-08-29 22:58:25 +0300

    (nvim): make `hx-on` query more specific

--- nvim/.config/nvim/queries/html/injections.scm
+++ nvim/.config/nvim/queries/html/injections.scm
@@ -2,7 +2,7 @@
 
 ; js highlighting for htmx `hx-on` attribute value
 (attribute 
-  (attribute_name) @_attr (#match? @_attr "hx-on:.*")
+  (attribute_name) @_attr (#match? @_attr "^hx-on:.*$")
   (quoted_attribute_value (attribute_value) @injection.content
   (#set! injection.language "javascript"))
 )
