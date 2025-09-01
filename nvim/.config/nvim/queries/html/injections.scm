5fdf4e01b4a7fa8eaa6311113d930883b5cb45ca
Niko Heiskanen <niko.heiskanen@kuulto.com>
2025-09-01 09:35:53 +0300

    (nvim): add more htmx queries

--- nvim/.config/nvim/queries/html/injections.scm
+++ nvim/.config/nvim/queries/html/injections.scm
@@ -7,7 +7,24 @@
   (#set! injection.language "javascript"))
 )
 
-; angular js highligting
+; highlighting for htmx `hx-vals` attribute
+
+; the parsing shows a bunch of errors but looks just fine
+(attribute 
+  (attribute_name) @_attr (#match? @_attr "^hx-vals")
+  (quoted_attribute_value
+    (attribute_value) @injection.content
+    (#match? @injection.content "^js:")
+    (#offset! @injection.content 0 3 0 0)
+    (#set! injection.language "javascript")))
+
+(attribute 
+  (attribute_name) @_attr (#match? @_attr "^hx-vals")
+  (quoted_attribute_value
+    (attribute_value) @injection.content
+    (#set! injection.language "json")))
+
+; angular js highlighting
 (attribute 
   (attribute_name) @_attr (#match? @_attr "\*ngIf")
   (quoted_attribute_value (attribute_value) @injection.content
