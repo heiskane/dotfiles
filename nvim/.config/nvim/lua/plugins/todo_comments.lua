99e31aef9e0915ac6f062fb6b1a685c7d3b5988f
Niko Heiskanen <niko.heiskanen@kuulto.com>
2025-09-11 13:03:31 +0300

    (nvim): update todo-comments highlights

--- nvim/.config/nvim/lua/plugins/todo_comments.lua
+++ nvim/.config/nvim/lua/plugins/todo_comments.lua
@@ -2,8 +2,16 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
-        highlight = { pattern = { [[.*\@<(KEYWORDS)\s*]], [[.*<(KEYWORDS)\s*:]] } },
-        search = { pattern = [[\b(KEYWORDS):|@\b(KEYWORDS)]] },
+        highlight = {
+            pattern = {
+                [[.*(\@(KEYWORDS))\s*]], -- matches: @TODO  comment
+                [[.*<((KEYWORDS)%(\(.{-1,}\))?)]], -- matches: TODO(potato): comment
+                [[.*<(KEYWORDS)\s*]], -- matches: TODO: comment
+            },
+        },
+        search = {
+            pattern = [[\b(KEYWORDS):|@\b(KEYWORDS)|\b(KEYWORDS)(\(\w*\))*:]],
+        },
     },
     keys = {
         { "<leader>to", "<cmd>TodoTelescope keywords=TODO,FIX<CR>", mode = "n" },
