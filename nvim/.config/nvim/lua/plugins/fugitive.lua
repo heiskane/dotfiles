b9a2a66baade4f302d603bb165b45c8e26050331
Niko Heiskanen <niko.heiskanen@kuulto.com>
2025-10-14 12:18:31 +0300

    (nvim): add command to create git tags

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,6 +1,6 @@
 return {
     "tpope/vim-fugitive",
-    cmd = { "Git", "Gwip" },
+    cmd = { "Git", "Gwip", "Gtag" },
     keys = {
         { "<leader>gi", "<cmd>vertical Git<cr>", mode = "n" },
         { "<leader>gl", "<cmd>vertical Git log --pretty=format:'%h %as %an %s'<CR>", mode = "n" },
@@ -9,8 +9,11 @@ return {
         vim.api.nvim_create_user_command("Gwip", function()
             vim.cmd([[Git add --all]])
             vim.cmd(string.format("Git commit -m '(wip): %s'", os.date("%Y-%m-%dT%H:%M")))
-        end, {
-            desc = "Create a `work_in_progress` commit",
-        })
+        end, { desc = "Create a `work_in_progress` commit" })
+
+        vim.api.nvim_create_user_command("Gtag", function(params)
+            vim.cmd(string.format("Git tag %s'", params.args))
+            vim.cmd(string.format("Git push origin tag %s'", params.args))
+        end, { desc = "Create git tag and push to origin", nargs = 1 })
     end,
 }
