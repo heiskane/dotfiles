0d0182c3b4930444b7597e3987e424139c1df3d4
Niko Heiskanen <niko.heiskanen@kuulto.com>
2025-11-24 09:34:45 +0200

    disable `names` option in nvim-colorizer

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -29,6 +29,10 @@ return {
     {
         "catgoose/nvim-colorizer.lua",
         event = "BufReadPre",
-        opts = {},
+        opts = {
+            user_default_options = {
+                names = false,
+            },
+        },
     },
 }
